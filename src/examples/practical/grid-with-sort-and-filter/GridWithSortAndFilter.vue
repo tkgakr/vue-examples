2e1b70957d3ce4ca820701f955296bc3a8ae1449
Akira Takagi <tkgakrwork@gmail.com>
2026-05-24 11:54:13 +0900

    example: Grid with Sort and Filter

---
+++ src/examples/practical/grid-with-sort-and-filter/GridWithSortAndFilter.vue
@@ -0,0 +1,30 @@
+<!--
+再利用可能なグリッドコンポーネントを作成し、外部データと組み合わせて使用する例。
+-->
+<script setup>
+import { ref } from 'vue'
+import DemoGrid from './Grid.vue'
+
+const searchQuery = ref('')
+const gridColumns = ['name', 'power']
+const gridData = [
+  { name: 'Chuck Norris', power: Infinity },
+  { name: 'Bruce Lee', power: 9000 },
+  { name: 'Jackie Chan', power: 7000 },
+  { name: 'Jet Li', power: 8000 },
+]
+</script>
+
+<template>
+  <form id="search">
+    Search <input
+      v-model="searchQuery"
+      name="query"
+    >
+  </form>
+  <DemoGrid
+    :data="gridData"
+    :columns="gridColumns"
+    :filter-key="searchQuery"
+  />
+</template>
